2aeb9c8cff0a78f27002134a00afec5991825a7e
Jamie Cerexhe <jcerexhe@gmail.com>
2017-03-19 10:02:18 +1100

    ffahackathon5 pre-deployment

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # FFA Hackathon Submission
 
-## Mission: Get people excited by women's football
+### Brief
 
-Brief: To improve the ability, access and pathway for females to become involved in the game of football as a player, coach, referee or volunteer
+- To improve the ability, access and pathway for females to become involved in the game of football as a player, coach, referee or volunteer
+- To bring football fans closer to the action and enable greater digital fan engagement with the Westfield W-League
+
+### Solution
+
+To solve the above objectives I identified a lack of user generated content which highlighted female players as a key difference between the male and female codes. This project is a prototype of a platform which would incentivise content creators who are fans of the sport to build up content highlighting female players. People could then use this website to discover new players to follow and to see how exciting women's football can be to watch.
 
 ### Installation
+
+Rails version 5.0.2
 
 ```
 $ git clone git@github.com:jcerexhe/ffahackathon.git
@@ -26,4 +33,6 @@ $ rails db:create
 $ rails db:migrate
 ```
 
-Todo: Create seeds file
+```
+$ rails db:seeds
+```
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # Create a user first!!
-player1 = Player.create(first_name: 'Michelle', last_name: 'Heyman', hometown: 'Shellharbour, Australia', current_team: 'Canberra United', position: 'Striker', position_type: 'Forward')
-# video1 = Video.create(title: 'Michelle Heyman Vs Japan // Olympic qualifiers ⊕ 2016', video_url: 'https://www.youtube.com/watch?v=exRlK_3dqsQ', user_id: 1, player_id: 1)
+admin = User.create(email: 'admin@ffa.com', password: 'womensfootballweek', admin: true)
+puts 'Created admin'
+
+player1 = Player.create(first_name: 'Michelle', last_name: 'Heyman', hometown: 'Shellharbour, Australia', current_team: 'Canberra United', position: 'Striker', position_type: 'Forward', photo: 'http://www1.pictures.zimbio.com/gi/Michelle+Heyman+Canberra+United+Headshots+C8boW4X81mFl.jpg')
+player2 = Player.create(first_name: "Yukari", last_name: "Kinga", hometown: "Yokohama, Japan", current_team: "Canberra United", position: "Defender", position_type: "Defender", photo: 'http://img.fifa.com/images/fwwc/2015/players/prt-3/190316.png')
+player3 = Player.create(first_name: "Ashleigh", last_name: "Sykes", hometown: "Dubbo, Australia", current_team: "Canberra United", position: "Striker", position_type: "Forward", photo: 'http://www2.pictures.zimbio.com/gi/Canberra+United+Headshots+Session+NBEdNwpxADAl.jpg')
+puts 'Created players'
+
+video1 = Video.create(title: 'Michelle Heyman Vs Japan // Olympic qualifiers ⊕ 2016', video_url: 'exRlK_3dqsQ', user_id: 1, player_id: 1)
+video2 = Video.create(title: "Ref gets an assist for Australia’s Michelle Heyman...", video_url: "r0KcvwREBx4", user_id: 1, player_id: 1)
+video3 = Video.create(title: "Yukari Kinga Goal　/　INAC神戸　近賀選手　先制ゴール　皇后杯三回戦　愛媛FC戦", video_url: "Gfu78J16hHA", user_id: 1, player_id: 2)
+video4 = Video.create(title: "Top 10 Goals Australian Matildas 2016", video_url: "Q_gac8WW7EA", user_id: 1, player_id: 3)
+puts 'Created videos'
